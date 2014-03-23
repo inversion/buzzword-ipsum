3070a00358839aee545eb291df50ce5785b74bf9
Andrew Moss <mossy1278@gmail.com>
2014-03-23 00:44:24 +0000

    reformatted html, added ajax loading text and image (fixes #14), deleted some silly comments I'd added to the html, changed tag used for footer to <footer>, other minor layout tweaks.

--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function () {
   'use strict';
 
+
   $('form#getText').submit(function(event) {
+    $('#textOut').html('<p>Leveraging strategic architecture...</p><img src="images/ajax-loader.gif" alt="AJAX Loading Image" />');
     $.ajax({
       url: '/buzzwords?format=html',
       data: $(this).serialize()
     }).done(function( data, textStatus, jqXHR ) {
-      $('div#textOut').html(data);
+      $('#textOut').html(data);
     });
     event.preventDefault();
   });
