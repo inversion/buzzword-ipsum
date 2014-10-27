532a00c9a1ea9c53006f7ea3e7063a8223af563e
Andrew Moss <andrew@mossage.co.uk>
2014-10-27 22:09:31 +0000

    Update jenkins_build.sh

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -3,5 +3,5 @@ export WORKON_HOME=/usr/local/virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
 workon buzzwordipsum
 npm install
-python setup.py develop
+cd webapp && python setup.py develop
 grunt deploy --wsgiTarget=$WSGI_TARGET --staticTarget=$STATIC_TARGET
