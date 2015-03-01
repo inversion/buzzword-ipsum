06304507fb700f1c9101e62baed048e825c71925
Raenn <danbelsey@hotmail.co.uk>
2015-03-01 12:48:11 +0000

    Removing the symbol from a sentence as the encoding started confusing it again

--- webapp/buzzwordipsum/sentencegenerator.py
+++ webapp/buzzwordipsum/sentencegenerator.py
@@ -100,5 +100,5 @@ class Sentences(object):
                     'It\'s critical that we give 110% when [adverb] [verb, PARTICIPLE] [noun, PLURAL].',
                     'Our [noun] development lifecycle enables [adjective], [adjective] [noun, PLURAL].',
                     'We use our [adjective] [noun, PLURAL] to [adverb] manage our [noun] expectations.',
-                    'Our [adjective] [noun]™ offers [noun, PLURAL] a suite of [adjective] offerings.']
+                    'Our [Adjective] [Noun] solution offers [noun, PLURAL] a suite of [adjective] offerings.']
 
