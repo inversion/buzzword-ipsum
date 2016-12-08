c367bf29fd2dcee0cc7310c9f38559b7da318b8f
Andrew Moss <andrew@mossage.co.uk>
2016-12-08 22:34:10 +0000

    Use yarn :cat2: add errexit to build script

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+set -o xtrace
+
 export WORKON_HOME=/usr/local/virtualenvs
 source /usr/local/bin/virtualenvwrapper.sh
+
 workon $VENV_NAME
-npm install
+set -o errexit
+yarn install
 cd webapp && python setup.py develop
 grunt deploy --wsgiTarget=$WSGI_TARGET --staticTarget=$STATIC_TARGET
